1d4d7225f4e33eec6074be8073feb8bba557e2bf
Shiva Sharma <shivasharmajpr2003@gmail.com>
2025-03-26 14:42:14 +0530

    4 program done

---
+++ src/SumOfNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SumOfNumbers {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the total numbers : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int i = 0;
+        while(i < n)
+        {
+            System.out.println("Enter the " + i + " number : ");
+            int num = sc.nextInt();
+            sum = sum + num;
+            i++;
+        }
+
+        System.out.println("Total sum of numbers is " + sum);
+    }
+}
