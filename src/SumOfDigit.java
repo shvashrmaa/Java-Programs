491a0ec30160eb01ee075e7506f33fd5d0c73b11
Shiva Sharma <shivasharmajpr2003@gmail.com>
2025-03-26 15:22:09 +0530

    6 program done

---
+++ src/SumOfDigit.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SumOfDigit {
+
+    public static int sumOfDigit(int num)
+    {
+        int sum = 0;
+        while(num>0)
+        {
+            if(num%10 != 0)
+            {
+                sum = sum + num % 10;
+                num = num / 10;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        System.out.println("Sum of digit of integer is : " + sumOfDigit(num));
+    }
+
+
+}
